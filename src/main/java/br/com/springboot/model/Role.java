b6973f4456b147b6eaa1e581276a1fe263e5bcdc
diegodanniel <diego.diniz@novaandradina.org>
2019-04-09 17:50:59 -0300

    primeiro commit

---
+++ src/main/java/br/com/springboot/model/Role.java
@@ -0,0 +1,26 @@
+package br.com.springboot.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
